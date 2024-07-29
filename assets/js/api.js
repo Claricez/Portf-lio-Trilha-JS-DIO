8c0a013e49d2a7b4cb9722a72222f508801d5eee
Claricez <clarmendzz@gmail.com>
2024-07-29 18:40:49 -0300

    update caminho raw profile.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fecthProfileData() {
   const url =
-    "https://raw.githubusercontent.com/RenanJPaula/js-developer-portfolio/main/data/profile.json";
+    "https://raw.githubusercontent.com/Claricez/Portf-lio-Trilha-JS-DIO/main/data/profile.json?token=GHSAT0AAAAAACVFNBIJBPBN4BJUGPSHCROSZVIBQZQ";
   const fetching = await fetch(url);
   return await fetching.json();
 }
