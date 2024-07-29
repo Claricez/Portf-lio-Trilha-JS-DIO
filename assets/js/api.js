909c6934ca053ecbed4bc52bf2c42187a4ab3eda
Claricez <clarmendzz@gmail.com>
2024-07-29 18:31:17 -0300

    feat: comunicando api dos dados com o portifolio

---
+++ assets/js/api.js
@@ -0,0 +1,6 @@
+async function fecthProfileData() {
+  const url =
+    "https://raw.githubusercontent.com/RenanJPaula/js-developer-portfolio/main/data/profile.json";
+  const fetching = await fetch(url);
+  return await fetching.json();
+}
